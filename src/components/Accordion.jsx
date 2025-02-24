f7faa51b300f0934f01fbf0f33332a16f354a83c
Chand-007 <bharathchandgetla@gmail.com>
2025-02-24 10:28:40 +0530

    lifting up state

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,12 +1,13 @@
-
+import { useState } from "react"
 import { data } from "./Data"
 import OneAccordion from "./OneAccordion"
 
 export default function Accordion(){
+    const [activeIndex,setActiveIndex] = useState(0)
 
     return(
         <div className="main-container">
-            {data.map((item)=>(<OneAccordion key={item.id} id={item.id} title={item.title} content={item.body}/>))}
+            {data.map((item)=>(<OneAccordion key={item.id} id={item.id} title={item.title} content={item.body} isActive={activeIndex === item.id} setActiveIndex={setActiveIndex} onShow={()=>setActiveIndex(item.id)}/>))}
         </div>
     )
 }
--- src/components/OneAccordion.jsx
+++ src/components/OneAccordion.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 import '../styles/Accordion.css'
 
 
-export default function OneAccordion({id,title,content}){
-    const [active,setActive] = useState(false)
+export default function OneAccordion({id,title,content,isActive,setActiveIndex,onShow}){
             return(
                 <div className="accordion-container" key={id}>
                     <div className="accordion-title" >
                         <p>{title}</p>
-                        <button className="acc-button" onClick={()=>setActive(prev => !prev)}>{active?"hide":"show"}</button>
+                        <button className="acc-button" onClick={onShow}>{isActive?"hide":"show"}</button>
                     </div>
-                    {active && (<div className="accordion-body">{content}</div>)}
+                    {isActive && (<div className="accordion-body">{content}</div>)}
                     
                 </div>
 
